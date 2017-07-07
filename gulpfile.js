d026304d4bf56d8a9a3bac9b0cd7c5f7b88d1336
LeidyRosmery <rosm3rita@hotmail.com>
2017-07-07 10:58:31 -0500

    tarea sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
 'use strict';
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+
 const config = {
-	source: "./src/",
-	dist: "./public/"
+    source: "./src/",
+    dist: "./public/"
 };
 
 
 const paths = {
-	html: "/*.html",
+    html: "/*.html",
+    sass: "scss/**/*.scss",
+    assets: "assets/",
+    mainSass: "scss/style.scss"
 };
 
 const sources = {
-	html: config.source + paths.html,
+    html: config.source + paths.html,
+    rootSass: config.source + paths.assets + paths.mainSass,
+    sass: paths.assets + paths.sass,
+    assets: config.source + paths.assets
 };
 
-gulp.task('html', ()=>{
-	gulp.src(sources.html)
-	.pipe(gulp.dest(config.dist));
+gulp.task('html', () => {
+    gulp.src(sources.html)
+        .pipe(gulp.dest(config.dist));
+});
+
+gulp.task('sass', ()=>{
+	gulp.src(sources.rootSass)
+	.pipe(sass({
+			outputStyle: "compressed"
+		}).on("Error", sass.logError))
+	.pipe(gulp.dest(config.dist+paths.assets+"css"));
 });
