8dfbb488c15d36d82d102ba69f4d309199b384cd
LeidyRosmery <rosm3rita@hotmail.com>
2017-07-07 11:14:46 -0500

    tarea js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+
+const concat = require('gulp-concat');
 
 const config = {
     source: "./src/",
@@ -9,17 +13,26 @@ const config = {
 
 
 const paths = {
-    html: "/*.html",
-    sass: "scss/**/*.scss",
     assets: "assets/",
-    mainSass: "scss/style.scss"
+    html: "/*.html",
+    js: "js/**/*.js",
+    components: "js/components/**.js",
+    utils: "js/utils/**.js",
+    sass: "scss/**/*.scss",
+    mainSass: "scss/style.scss",
+    mainJs: "js/index.js"
 };
 
 const sources = {
+
+    assets: config.source + paths.assets,
     html: config.source + paths.html,
-    rootSass: config.source + paths.assets + paths.mainSass,
     sass: paths.assets + paths.sass,
-    assets: config.source + paths.assets
+    js: paths.assets + paths.js,
+    components: config.source + paths.assets + paths.components,
+    utils: config.source + paths.assets + paths.utils,
+    rootSass: config.source + paths.assets + paths.mainSass,
+    rootJs: config.source + paths.assets + paths.mainJs
 };
 
 gulp.task('html', () => {
@@ -27,10 +40,18 @@ gulp.task('html', () => {
         .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('sass', ()=>{
-	gulp.src(sources.rootSass)
-	.pipe(sass({
-			outputStyle: "compressed"
-		}).on("Error", sass.logError))
-	.pipe(gulp.dest(config.dist+paths.assets+"css"));
+gulp.task('sass', () => {
+    gulp.src(sources.rootSass)
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on("Error", sass.logError))
+        .pipe(gulp.dest(config.dist + paths.assets + "css"));
+});
+
+
+gulp.task('js', () => {
+    gulp.src(["./src/assets/js/components/principal.js", "./src/assets/js/components/header.js", "./src/assets/js/utils/get-json.js", "./src/assets/js/index.js"])
+        .pipe(concat('index.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest(config.dist + paths.assets + "js"))
 });
