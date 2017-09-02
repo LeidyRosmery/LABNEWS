761d1a589631e6730ee725dad0226b07a57d80fe
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-02 02:03:08 -0500

    title news principal

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -75,15 +75,15 @@ const Principal = () => {
     const tituloSeccion = $(' <div class="hidden-lg pd col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title"> LO ULTIMO</h3><hr class="category--underline"></div> ');
     const divImg1 = $("<div class='pd col-lg-12 col-md-12 col-xs-12 col-sm-12 '>");
     const img1 = $('<img class="news news--img__lg img--lg img-responsive" src="assets/img/' + laboratoriaNews.news[0].img + '" alt="">');
-    const divText1 = $("<div class='hidden-xs text-justify col-lg-8 col-md-12 col-xs-12 col-sm-12 newsInf'>");
+    const divText1 = $("<div class=' text-justify col-lg-8 col-md-12 col-xs-12 col-sm-12 newsInf'>");
     const title1 = $('<h2 class="title-news"> ' + laboratoriaNews.news[0].title + '</h2>');
-    const text1 = $('<p  class="newsInf--text hidden-xs"> ' + laboratoriaNews.news[0].brief + '</p>');
+    const text1 = $('<p  class="hidden-xs newsInf--text hidden-xs"> ' + laboratoriaNews.news[0].brief + '</p>');
     principal.append(container);
     container.append(row);
     row.append(tituloSeccion);
     row.append(divImg1);
     divImg1.append(img1);
-    row.append(divText1);
+    divImg1.append(divText1);
     divText1.append(title1);
     divText1.append(text1);
 
