b932c1afbe2628a966efe02fdfa022042e43445f
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-05 22:14:58 -0500

    responsive article movil

--- src/assets/js/components/detailNews.js
+++ src/assets/js/components/detailNews.js
@@ -1,24 +1,28 @@
 const DetailNews = (update) => {
     const detailNews = $('<section class="educacion padBoth">');
     const container = $('<div class="container-fluid ">');
-    const row = $("<div class='row'>");
-    const hr1 = $('<hr class="article--borde">');
-    const tituloSeccion = $(' <div class="pd col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title">EDUCACION</h3><hr class="category--underline"></div> ');
+    const row = $("<div class='row pdx1'>");
+    const hr1 = $('<hr class="hidden-xs article--borde">');
+    const tituloSeccion = $(' <div class="pd hidden-xs col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title">EDUCACION</h3><hr class="category--underline"></div> ');
     const divImg1 = $("<div class='pd col-lg-12 col-md-12 col-xs-12 col-sm-12 '>");
     const img1 = $('<img class="news news--img__lg img--lg img-responsive" src="assets/img/foto-n1.png" alt="">');
-    const hr2 = $('<hr class="article--borde">');
-    const divText1 = $("<div class=' text-justify col-lg-9 col-md-12 col-xs-12 col-sm-12'>");
+    const hr2 = $('<hr class="article--borde pdx1">');
+    const row3 =$("<div class='row'>");
+    const divText1 = $("<div class='title--article text-left col-lg-9 col-md-12 col-xs-12 col-sm-12'>");
     const title1 = $('<h2 class=""> ' + laboratoriaNews.news[0].title + '</h2>');
-    const text1 = $('<p  class="hidden-xs  hidden-xs"><strong> ' + laboratoriaNews.news[0].brief + '</strong></p>');
+    const divTexts = $("<div class='title--article text-left col-lg-6 col-md-12 col-xs-12 col-sm-12'>");
+    const text1 = $('<p  class="subtitle--article"><strong> ' + laboratoriaNews.news[0].brief + '</strong></p>');
 
     detailNews.append(container)
     container.append(row);
     row.append(hr1);
     row.append(tituloSeccion);
     row.append(divImg1);
-    divImg1.append(divText1);
+    divImg1.append(row3);
+    row3.append(divText1);
     divText1.append(title1);
-    divText1.append(text1);
+    row3.append(divTexts);
+    divTexts.append(text1);
     divImg1.append(img1);
     row.append(hr2);
     const row2 = $("<div class='row'>");
@@ -26,40 +30,90 @@ const DetailNews = (update) => {
     const news = $(laboratoriaNews.selectedNews.body);
     const newsText = laboratoriaNews.selectedNews.body.split("</p>");
     const news0 = (boldFont(changeColor(boldFont(newsText[0], 27, 45), 49, 52), 5, 11) + '</p>');
-    const news1 = $(newsText[1]+ '</p>');
-    const news2 = $(newsText[2]+ '</p>');
-    
-    const news3 = $(newsText[3]+ '</p>');
+    const news1 = $(newsText[1] + '</p>');
+    const news2 = $(newsText[2] + '</p>');
+
+    const XSrelationNews = $('<div class="hidden-lg row"></div>');
+    const XStitleRelationNews = $(' <div class="pdx1  col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title">NOTICIAS RELACIONADAS</h3><hr class="category--underline"></div><br> ');
+    const XScol1 = $('<div class="col-lg-6 col-xs-6 "></div>');
+    const XSimgRelation1 = $('<img class=" img-responsive" src="assets/img/foto-n3.png" alt="">');
+    const XScol2 = $('<div class="col-lg-6 col-xs-6 "></div>');
+    const XSimgRelation2 = $('<img class=" img-responsive" src="assets/img/foto-n4.png" alt="">');
+    const XStextTitle2 = $('<p class="">Laboratoria 2017 Titulo Noticia</p>');
+    const XStextTitle3 = $('<p class="">Laboratoria 2017 Titulo Noticia</p>');
+
+
+
+
+
+    const news3 = $(newsText[3] + '</p>');
     const news4 = $(boldFont(newsText[4], 25, 60) + '</p>');
-    const news5 = $(newsText[5]+ '</p>');
-    const news6 = $(newsText[6]+ '</p>');
+    const news5 = $(newsText[5] + '</p>');
+
+    const XSpopularNews = $('<div class="hidden-lg row"></div>');
+
+    const XSsubItem1 = $('<div class="col-lg-12  col-xl-12  col-sm-12 col-xs-12 ">');
+    const XSimgSubItem1 = $('<img class="news imgArticle img-responsive img--sm" src="assets/img/foto-n5.png" alt="">');
+    const XSdivTextSubItem1 = $('<div class="detailArticle  news--detail__sm detail--sm col-lg-12  col-xl-12  col-sm-12 col-xs-12">');
+    const XStextSubItem1 = $('<p class="">Noticia mas popular Laboratoria 2017</p>');
+    const XStitleRelationNews1 = $(' <div class="pdx1  col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title">NOTICIAS POPULARES</h3><hr class="category--underline"></div><br><br> ');
+
+
+    const XSsubItem2 = $('<div class="col-lg-12  col-xl-12  col-sm-12 col-xs-12 ">');
+    const XSimgSubItem2 = $('<img class="news imgArticle  img-responsive img--sm" src="assets/img/foto-n6.png" alt="">');
+    const XSdivTextSubItem2 = $('<div class="detailArticle  news--detail__sm detail--sm col-lg-12  col-xl-12  col-sm-12 col-xs-12">');
+    const XStextSubItem2 = $('<p class="">Noticia mas popular Laboratoria 2017</p>');
+
+
+    const news6 = $(newsText[6] + '</p>');
     const news7 = $(changeColor(newsText[7], 3, 10) + '</p>');
     const colRight = $('<div class="pd text-justify col-lg-9 col-md-12 col-xs-12 ">');
     const divPerfil = $('<div class="pd text-justify col-lg-12 col-md-12 col-xs-12 ">');
+    const divLeft = $('<div class="pd text-justify col-lg-1 col-md-12 col-xs-4 ">');
     const imgPerfil = $('<img class="perfil" src="assets/img/dianaluque.png" alt="">');
-    const person = $("<span>DIANA LUQUE LAVADO </span>");
-    const date = $("<span>22.06.17 12:36 pm </span>");
-    const socialMedia = $("<span>@photosynthetica</span>");
+    const divRight = $('<div class=" text-left col-lg-9 col-md-12 col-xs-8 ">');
+    const person = $("<p>DIANA LUQUE LAVADO </p>");
+    const date = $("<p>22.06.17 12:36 pm </p>");
+    const socialMedia = $("<p>@photosynthetica</p>");
     container.append(row2);
-
     row2.append(colRight);
     row2.append(colLeft);
     colRight.append(divPerfil);
-    divPerfil.append(imgPerfil);
-    divPerfil.append(person);
-    divPerfil.append(date);
-    divPerfil.append(socialMedia);
+    divPerfil.append(divLeft)
+    divLeft.append(imgPerfil);
+      divPerfil.append(divRight)
+    divRight.append(person);
+    divRight.append(date);
+    divRight.append(socialMedia);
     colRight.append(news0);
     colRight.append(news1);
     colRight.append(news2);
+    colRight.append(XSrelationNews);
+    XSrelationNews.append(XStitleRelationNews);
+    XSrelationNews.append(XScol1);
+    XSrelationNews.append(XScol2);
+    XScol1.append(XSimgRelation1);
+    XScol2.append(XSimgRelation2);
+    XScol1.append(XStextTitle2);
+    XScol2.append(XStextTitle3);
     colRight.append(news3);
     colRight.append(news4);
     colRight.append(news5);
+    colRight.append(XSpopularNews);
+    XSpopularNews.append(XStitleRelationNews1);
+    XSpopularNews.append(XSsubItem1);
+    XSsubItem1.append(XSimgSubItem1);
+    XSsubItem1.append(XSdivTextSubItem1);
+    XSdivTextSubItem1.append(XStextSubItem1);
+    XSpopularNews.append(XSsubItem2);
+    XSsubItem2.append(XSimgSubItem2);
+    XSsubItem2.append(XSdivTextSubItem2);
+    XSdivTextSubItem2.append(XStextSubItem2);
     colRight.append(news6);
     colRight.append(news7);
 
 
-    const relationNews = $('<div class="row"></div>');
+    const relationNews = $('<div class="row hidden-xs"></div>');
     const titleRelationNews = $(' <div class="pdx1  col-lg-12 col-md-12 col-xs-12 "><h3 class="category--title">NOTICIAS RELACIONADAS</h3><hr class="category--underline"></div><br> ');
     const col1 = $('<div class="col-lg-6 "></div>');
     const imgRelation1 = $('<img class=" img-responsive" src="assets/img/foto-n3.png" alt="">');
@@ -76,7 +130,7 @@ const DetailNews = (update) => {
     col1.append(textTitle2);
     col2.append(textTitle3);
 
-    const popularNews = $('<div class="row"></div>');
+    const popularNews = $('<div class="hidden-xs row"></div>');
     colLeft.append(popularNews);
     const subItem1 = $('<div class="col-lg-12  col-xl-12  col-sm-12 col-xs-12 ">');
     const imgSubItem1 = $('<img class="news imgArticle img-responsive img--sm" src="assets/img/foto-n5.png" alt="">');
